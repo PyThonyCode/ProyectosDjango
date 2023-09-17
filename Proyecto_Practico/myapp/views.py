c3752702ddae0826301d407a83bba02e211ff294
PyThonyCode <thonylp10@gmail.com>
2023-09-17 18:55:51 -0500

    actualizado

--- Proyecto_Practico/myapp/views.py
+++ Proyecto_Practico/myapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def hello(request):
+    return HttpResponse("<h1>HOLA MUNDO</h1>")
+
+
+def about(request):
+    return HttpResponse("<h2>ABOUT</h2>")
